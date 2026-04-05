70917033cbf40d0cc103b83fdefcdaaf94e92295
LINTANG PUTRI_ANDINI_NAME <lintangandini836@gmail.com>
2026-04-05 21:21:25 +0700

    Update ListBarangController.php

--- app/Http/Controllers/ListBarangController.php
+++ app/Http/Controllers/ListBarangController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class ListBarangController extends Controller
 {
-    public function tampilkan($id, $nama) {
-        return view('list_barang', ['id' => $id, 'nama' => $nama]);
-        
+    public function index()
+    {
+        $barangs = [
+            ['id' => 1, 'nama' => 'Laptop'],
+            ['id' => 2, 'nama' => 'Mouse'],
+            ['id' => 3, 'nama' => 'Keyboard'],
+        ];
+    }
+        return view('list_barang', compact('barangs'));
     }
 }
